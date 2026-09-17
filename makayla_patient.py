856d30610492586c9f563caaaf3cb49d57eb5817
Athena20227876 <vnt7aj@virginia.edu>
2026-09-16 22:03:53 -0400

    Finished adding comments to complete file

--- makayla_patient.py
+++ makayla_patient.py
@@ -1,13 +1,13 @@
-import csv
+import csv # importing the csv module to read the .csv file
 
-class Patient:
-    all_patients = []
+class Patient: # defining the patient class
+    all_patients = [] # creating a list to hold all the patient objects that will be created from the .csv file
 
-    @classmethod
-    def filter(cls, list, age_at_death:int = "any", sex:str = "any", highest_education_level:str = "any", known_head_injury:str = "any",Amyloid_Beta42_levels:int= "any",Tau_levels:int = "any" ):
-            all_patients = list
-            remove_list = []
-            attr_list = (
+    @classmethod # defining a class method that will filter the patient objects based on the attributes provided in the data set
+    def filter(cls, list, age_at_death:int = "any", sex:str = "any", highest_education_level:str = "any", known_head_injury:str = "any",Amyloid_Beta42_levels:int= "any",Tau_levels:int = "any" ): # inputting the attributes that will be used to filter the patient objects, with default values of "any" if no value is provided
+            all_patients = list # the patients will be filtered from the list of all patients
+            remove_list = [] # creating a list to hold the patient objects that will be removed from the list of all patients
+            attr_list = ( # creating a tuple to hold the attributes that will be used to filter the patient objects
                         age_at_death,
                         sex,
                         highest_education_level,
@@ -15,7 +15,7 @@ class Patient:
                         Amyloid_Beta42_levels,
                         Tau_levels
                         )
-            attr_name = (
+            attr_name = ( # creating a tuple to hold the names of the attributes that will be used to filter the patient objects
                         "age_at_death",
                         "sex",
                         "highest_education_level",
@@ -23,24 +23,24 @@ class Patient:
                         "Amyloid_Beta42_levels",
                         "Tau_levels"
                         )
-            for attr in range(len(attr_list)):
-                if attr_list[attr] != "any":
-                    for patient in all_patients:
-                        if getattr(patient, attr_name[attr]) != attr_list[attr]:
-                            remove_list.append(patient)
-                    all_patients = [patient for patient in all_patients if patient not in remove_list]
-                    remove_list.clear()
+            for attr in range(len(attr_list)): # looping through the attributes in the attr_list tuple to filter the patient objects based on the values provided in the data set
+                if attr_list[attr] != "any": # clarifying that if the value of the attribute is not "any", the patient objects will be filtered based on the value provided in the data set
+                    for patient in all_patients: # looping through the patient objects in the all_patients list to filter the patient objects based on the values provided in the data set
+                        if getattr(patient, attr_name[attr]) != attr_list[attr]: # clarifying that if the value of the attribute is not equal to the value provided in the data set, the patient object will be added to the remove_list
+                            remove_list.append(patient) 
+                    all_patients = [patient for patient in all_patients if patient not in remove_list] # clarifying that the patient objects in the remove_list will be removed from the all_patients list
+                    remove_list.clear() 
 
             return all_patients
     @classmethod
-    def instantiate_from_csv(cls, filename: str):
+    def instantiate_from_csv(cls, filename: str): # defining a class method that will read the .csv file and create patient objects based on the data provided in the .csv file
 
-         with open(filename, encoding="utf8") as f:
+         with open(filename, encoding="utf8") as f: # opening the .csv file and reading the data provided in it
             reader = csv.DictReader(f)
             rows_of_patients = list(reader)
 
-            for row in rows_of_patients:
-                    Patient(
+            for row in rows_of_patients: # creating a patient object for each row in the .csv file based on the data provided
+                    Patient( # creating a patient object based on the data provided in the .csv file
                     age_at_death = float(row['Age at Death']),
                     sex = row['Sex'],
                     highest_education_level = row['Highest level of education'],
@@ -49,22 +49,22 @@ class Patient:
                     Tau_levels = float(row['pTAU pg/ug'])
                 )
     @classmethod
-    def get_all_patients(cls):
+    def get_all_patients(cls): # defining a class method that will return the list of all patient objects that have been created
         return cls.all_patients
-    def __init__(self, age_at_death, sex, highest_education_level, known_head_injury, Amyloid_Beta42_levels, Tau_levels):
+    def __init__(self, age_at_death, sex, highest_education_level, known_head_injury, Amyloid_Beta42_levels, Tau_levels): # defining the constructor method that will create a patient object based on the data provided in the .csv file
         self.age_at_death = age_at_death
         self.sex = sex
         self.highest_education_level = highest_education_level
         self.known_head_injury = known_head_injury
         self.Amyloid_Beta42_levels = Amyloid_Beta42_levels
         self.Tau_levels = Tau_levels
-        Patient.all_patients.append(self)
+        Patient.all_patients.append(self) # adding the patient object to the list of all patient objects that have been created 
 
-    def __repr__(self):
+    def __repr__(self): # defining the representer method that will return a string representation of the patient object that has been created
         return f"Patient(age_at_death={self.age_at_death}, sex={self.sex}, highest_education_level={self.highest_education_level}, known_head_injury={self.known_head_injury}, Amyloid_Beta42_levels={self.Amyloid_Beta42_levels}, Tau_levels={self.Tau_levels})"
 
-    def get_age_at_death(self):
+    def get_age_at_death(self): # defining a method that will return the age at death of the patient
         return self.age_at_death
 
-    def get_Amyloid_Beta42_levels(self):
+    def get_Amyloid_Beta42_levels(self): # defining a method that will return the Amyloid-Beta42 levels of the patient
         return self.Amyloid_Beta42_levels
--- makayla_patient_class_main.py
+++ makayla_patient_class_main.py
@@ -1,6 +1,6 @@
-from makayla_patient import *
+from makayla_patient import * # import code from makayla_patient.py file
 
-Patient.instantiate_from_csv("C:/Users/makay/OneDrive - University of Virginia/BME 2315/Module 1/Metadata and Protein Data for Module 1.csv")
+Patient.instantiate_from_csv("C:/Users/makay/OneDrive - University of Virginia/BME 2315/Module 1/Metadata and Protein Data for Module 1.csv") # instantiate Patient objects from the .csv file
 
 # import packages that will be used in the analysis of the data
 import matplotlib.pyplot as plt # imports package for plotting data on a graph
@@ -8,54 +8,61 @@ from scipy import stats # imports package that statistically analyzes data
 import numpy as np # imports package allowing mathematical operations with the data
 import statistics # imports package that statistically analyzes data
 
-with open("C:/Users/makay/OneDrive - University of Virginia/BME 2315/Module 1/Metadata and Protein Data for Module 1.csv", newline="") as f:
+# open the .csv file and print the headers
+with open("C:/Users/makay/OneDrive - University of Virginia/BME 2315/Module 1/Metadata and Protein Data for Module 1.csv", newline="") as f: # opens the .csv file
         reader = csv.reader(f)
         headers = next(reader) # Get the first row
-        for h in headers:
+        for h in headers: # function to print the headers
             print(h)
 
-Patient.all_patients.sort(key=Patient.get_Amyloid_Beta42_levels, reverse=False)
+Patient.all_patients.sort(key=Patient.get_Amyloid_Beta42_levels, reverse=False) # sort patient data by Amyloid-Beta42 levels from lowest to highest
 
-for patient in Patient.all_patients:
+for patient in Patient.all_patients: # print the patient data in order of age onto terminal
       print(patient)
 
-filtered_patients = Patient.filter(
-      Patient.get_all_patients(),
+filtered_patients = Patient.filter( # filters the patient data by sex and education level
+      Patient.get_all_patients(), # from all patients filter out only those that are male and went to Trade or Tech School
       sex = "Male",
       highest_education_level = "Trade School/ Tech School"
 )
 
-print("\nMale patients who have completed Trade or Tech School are:")
+print("\nMale patients who have completed Trade or Tech School are:") # print out a message announcing what was just filtered and will be printed next
 
-for patient in filtered_patients:
+for patient in filtered_patients: # print the filtered patient data
       print(patient)
 
-Patient.all_patients.sort(key=Patient.get_age_at_death, reverse=False)
+Patient.all_patients.sort(key=Patient.get_age_at_death, reverse=False) # order the patient data by age at death from youngest to oldest
 
-for patient in Patient.all_patients:
+for patient in Patient.all_patients: # print the patient data in order of age at death
       print(patient)
 
 Patient.all_patients.sort(key=Patient.get_Amyloid_Beta42_levels, reverse=False)
 
+
+# creating a box graph for data visualization of Amyloid-Beta42 levels by sex
 f_ABeta42 = []
 m_ABeta42 = []
 
-for patient in Patient.all_patients:
-        if patient.sex == "Female":
-            f_ABeta42.append(patient.Amyloid_Beta42_levels)
-        elif patient.sex == "Male":
+for patient in Patient.all_patients: # function that will sort the Amyloid-Beta42 levels by sex and put them into separate lists
+        if patient.sex == "Female": # clarifying that if the patient is female, their Amyloid-Beta42 levels will be added to the female list
+            f_ABeta42.append(patient.Amyloid_Beta42_levels) 
+        elif patient.sex == "Male": # clarifying that if the patient is male, their Amyloid-Beta42 levels will be added to the male list
             m_ABeta42.append(patient.Amyloid_Beta42_levels)
 
+# taking the mean of both data sets
 f_mean = np.mean(f_ABeta42)
 m_mean = np.mean(m_ABeta42)
 
+# finding the standard deviation of both data sets
 f_standard_dev = np.std(f_ABeta42, ddof=1)
 m_standard_dev = np.std(m_ABeta42, ddof=1)
 
+# creating a bar graph for data visualization of Amyloid-Beta42 levels by sex
 sexes = ["Female", "Male"]
 means = [f_mean, m_mean]
 standard_devs = [f_standard_dev, m_standard_dev]
 
+# labeling the bar graph with the x-axis, y-axis, and title
 plt.bar(
       sexes,
       means,
@@ -67,22 +74,26 @@ plt.xlabel("Sex")
 plt.ylabel("Mean Amyloid-Beta42 (pg/ug)")
 plt.title("Amyloid-Beta42 Levels by Sex")
 
-plt.show()
+plt.show() # displays final graph
 
+# creating a scatter plot for data visualization of Amyloid-Beta42 levels vs. age at death
 patient.age_at_death
 patient.Amyloid_Beta42_levels
 
+# creating two lists to hold the data for the scatter plot
 age_at_death = []
 ABeta42_values = []
 
+# populating the two lists with the data from the patient objects in the data set
 for patient in Patient.all_patients:
       age_at_death.append(patient.age_at_death)
       ABeta42_values.append(patient.Amyloid_Beta42_levels)
 
-plt.scatter(age_at_death, ABeta42_values)
+plt.scatter(age_at_death, ABeta42_values) # clarifying the type of graph being used and which value is on the x or y axis
 
+# labeling the scatter plot with the x-axis, y-axis, and title
 plt.xlabel("Age at Death")
 plt.ylabel("Amyloid-Beta42 (pg/ug)")
 plt.title("Amyloid-Beta42 vs. Age at Death")
 
-plt.show()
+plt.show() # displays final scatterplot graph
